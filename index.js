699e5f72f6066a7994d10404f65830c181394bac
gucci1909 <umangarora0134@gmail.com>
2022-10-27 00:19:09 +0530

    portfolio

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
  $(window).scroll(function(){
+    if(this.scrollY > 2000){
+      $('.navbar').addClass("remove");
+    }  
     if(this.scrollY >20){
-        $('.navbar').addClass("sticky");
-
-    }else{
-        $(`.navbar`).removeClass("sticky")
+      $('.navbar').addClass("sticky");
+    }
+    else{
+        $(`.navbar`).removeClass("sticky");
+        $('.navbar').removeClass("remove");
 
     }
  })
@@ -12,4 +16,53 @@ $(document).ready(function(){
     $(`.navbar .menu`).toggleClass("active");
     $(`.menu-btn i`).toggleClass("active")
  })
+
+ var carousels = function () {
+    $(".owl-carousel1").owlCarousel({
+      loop: true,
+      center: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false
+        },
+        1000: {
+          items: 3,
+          nav: true
+        }
+      }
+    });
+    $(".owl-nav").owlCarousel({
+      loop: true,
+      center: true,
+      margin: 0,
+      fontSize: "300px",
+      responsiveClass: true,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false
+        },
+      }
+    });
+  };
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+
 })
